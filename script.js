3943b2fe8f86d9db5d5a2d0613e2d208d5d331f4
rosan777 <146227825+rosan777@users.noreply.github.com>
2024-02-04 20:00:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('last-modified').innerHTML = new Date(document.lastModified); //.toLocaleString()
+document.getElementById('Location').innerHTML = window.location.hostname; //window.location.pathname
+
